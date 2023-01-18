2d5667570a5be3c7590be72ca1e185fd9fe13379
acethecreator <devaze007@gmail.com>
2023-01-18 02:44:30 +0100

    wrote script for generarting spec oject

--- src/scripts/build-specs.ts
+++ src/scripts/build-specs.ts
@@ -1,6 +1,25 @@
-import { writeFileSync } from "fs";
-import { resolve } from "path";
+import fs, { writeFileSync, existsSync } from "fs";
+import path, { resolve } from "path";
 import fetch from "node-fetch";
+
+async function createDir(name: string){
+  if(!existsSync(`src/configs/${name}`)){
+    console.log('jfj')
+      fs.mkdir(path.join("src/configs", name), (err) => {
+        if (err) {
+          return console.error(err);
+        }
+        console.log("Directory created successfully!");
+      });
+  }
+}
+
+async function createFile(dir: string, name: string, data: string){
+  writeFileSync(
+    resolve(__dirname, `../configs/${dir}`, name),
+    data
+  );
+}
 
 export default async function buildSpecs() {
   try {
@@ -11,8 +30,21 @@ export default async function buildSpecs() {
       },
     });
 
-    const spec = await res.json();
-    console.log(spec);
+    const spec: any = await res.json();
+    const properties = spec.properties;
+    const definitions = spec.definitions;
+    const specVersion = properties.asyncapi.enum[0];     
+    if(properties){
+      // create spec directory
+      await createDir(specVersion)
+      // write all definations to spec directory
+      for (const definition in definitions) {
+        const title = definition.split("/").slice(-1)[0];
+        const scope = definitions[definition];
+       await createFile(specVersion, title, JSON.stringify(scope));
+      }
+    }
+
   } catch (error) {
     console.log(error);
   }
