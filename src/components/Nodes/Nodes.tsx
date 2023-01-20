608f01b547d945c484684ea12da63e07ebab3122
acethecreator <devaze007@gmail.com>
2023-01-20 13:38:59 +0100

    fixed tags nested scheme

--- src/components/Nodes/Nodes.tsx
+++ src/components/Nodes/Nodes.tsx
@@ -10,35 +10,6 @@ import ReactFlow, {
 import "reactflow/dist/style.css";
 import initialNodes from '../../configs/2.5.0.json';
 
-
-// const initialNodes = [
-//   {
-//     id: "1",
-//     name: "a",
-//     children: [
-//       {
-//         id: "2",
-//         name: "b",
-//         parent: "1",
-//         children: [
-//           {
-//             id: "3",
-//             name: "c",
-//             parent: "2",
-//             children: [
-//               {
-//                 id: "4",
-//                 parent: "3",
-//                 name: "d",
-//               },
-//             ],
-//           },
-//         ],
-//       },
-//     ],
-//   },
-// ];
-
 const initialEdges = [
   {
     id: "edges-e5-7",
@@ -146,8 +117,6 @@ const Nodes = (props: any) => {
         onConnect={onConnect}
         onNodeClick={handleNodeClick}
         fitView
-        onHov
-        maxZoom={0.9}
         defaultViewport={{ x: 1, y: 1, zoom: 0.5 }}
         fitViewOptions={fitViewOptions}
       />
--- src/scripts/build-tree.ts
+++ src/scripts/build-tree.ts
@@ -16,18 +16,9 @@ async function walkDirectories(directories: string[]) {
   for (let dir of directories) {
     let files = readdirSync(dir);
     for (let file of files) {
-      const fileName =
-        file.split(".")[0].charAt(0).toLowerCase() +
-        file.split(".")[0].slice(1);
       const filePath = [dir, file].join("/");
       const fileContent: any = fs.readFileSync(filePath, "utf-8");
       const data = JSON.parse(fileContent);
-      // console.log(filePath)
-      // if(!data.parent){
-      //     console.log('ok')
-      //     data.parent = 'asyncapi'
-      // }
-      // constructParentarray(fileName, data.properties)
     }
   }
 }
@@ -35,28 +26,52 @@ async function walkDirectories(directories: string[]) {
 function createSubTrees(parent: any, childPath: string) {
   const fileContent: any = fs.readFileSync(childPath, "utf-8");
   const data = JSON.parse(fileContent);
-  if(parent.properties){
-    console.log(true);
-  }else{
-    console.log(parent)
-  }
   parent = {
     ...parent,
     ...data,
   };
-  delete parent["$ref"];
   let props = parent.properties;
-  if(parent.additionalProperties){
-    const newProperties = parent.additionalProperties.oneOf;
-
-    
+  if (parent.additionalProperties) {
+    const additionalProperties =
+      parent.additionalProperties.oneOf || parent.additionalProperties.anyOf;
+    const additionalProperty = parent.additionalProperties["$ref"];
+    if (additionalProperties) {
+      const newProperties: any = {};
+      for (let i = 0; i < additionalProperties.length; i++) {
+        const newRef = additionalProperties[i]["$ref"].split("/").slice(-1)[0];
+        const filePath = `src/data/2.5.0/${newRef}`;
+        const fileContent: any = fs.readFileSync(filePath, "utf-8");
+        const data = JSON.parse(fileContent);
+        const properties = data.properties;
+        for (const prop in properties) {
+          newProperties[prop] = properties[prop];
+        }
+      }
+      parent = {
+        ...parent,
+        properties: newProperties,
+      };
+      props = parent.properties;
+    }
+    if (additionalProperty) {
+      const newRef = additionalProperty.split("/").slice(-1)[0];
+      const filePath = `src/data/2.5.0/${newRef}`;
+      const fileContent: any = fs.readFileSync(filePath, "utf-8");
+      const data = JSON.parse(fileContent);
+      parent = {
+        ...parent,
+        properties: data.properties,
+      };
+      props = parent.properties;
+    }
   }
   if (parent.children) {
-    createChildren()
+    createChildren();
   } else {
-    parent["children"] = [parent.properties];
+    parent["children"] = [parent.properties] || [];
     createChildren();
   }
+  delete parent["$ref"];
   delete parent.properties;
   function createChildren() {
     for (const prop in props) {
@@ -76,6 +91,13 @@ function createInitalTree() {
   const fileContent: any = fs.readFileSync(filePath, "utf-8");
   const data = JSON.parse(fileContent);
   for (const key in data) {
+    if(data[key].type === 'array' && data[key].items){
+        const items = data[key].items;
+        console.log(Object.values(items));
+    data[key][Object.keys(items)[0]] = Object.values(items)[0]
+    delete data[key].items;
+    }
+    console.log(data)
     res[0].children.push({
       ...data[key],
       parent: res[0].id,
@@ -86,22 +108,23 @@ function createInitalTree() {
   }
 }
 
-function getObject(theObject: any) {
+function getObject(theObject: any, key: string) {
   var result = null;
   if (theObject instanceof Array) {
     for (var i = 0; i < theObject.length; i++) {
-      result = getObject(theObject[i]);
+      result = getObject(theObject[i], key);
       if (result) {
         break;
       }
     }
   } else {
     for (var prop in theObject) {
-      if (prop == "$ref") {
-        const newRef = theObject[prop].split("/").slice(-1)[0];
-        theObject[prop] = `src/data/2.5.0/${newRef}`;
-        createSubTrees(theObject, theObject[prop]);
-        // console.log(theObject[prop]);
+      if (prop == key) {
+        if (key === "$ref") {
+          const newRef = theObject[prop].split("/").slice(-1)[0];
+          theObject[prop] = `src/data/2.5.0/${newRef}`;
+          createSubTrees(theObject, theObject[prop]);
+        }
         if (theObject[prop] == 1) {
           return theObject;
         }
@@ -110,7 +133,7 @@ function getObject(theObject: any) {
         theObject[prop] instanceof Object ||
         theObject[prop] instanceof Array
       ) {
-        result = getObject(theObject[prop]);
+        result = getObject(theObject[prop], key);
         if (result) {
           break;
         }
@@ -122,12 +145,10 @@ function getObject(theObject: any) {
 
 export default async function buildTree() {
   createInitalTree();
-  const result = getObject(res);
-
-  //   console.log(result)
+  getObject(res, "$ref");
+  getObject(res, "additionalProperties");
 
   await walkDirectories(specDirectories);
-  // console.log(res)
   writeFileSync(
     resolve(__dirname, `../configs`, "2.5.0.json"),
     JSON.stringify(res)
