ba2746e32c9c064df4b3ffac9426fb4409640761
acethecreator <devaze007@gmail.com>
2023-01-25 02:24:46 +0100

    laying out node on handleClick

--- src/components/Nodes/Nodes.tsx
+++ src/components/Nodes/Nodes.tsx
@@ -7,9 +7,14 @@ import ReactFlow, {
   addEdge,
   ReactFlowProvider,
   MarkerType,
+  useReactFlow,
+  useStoreApi,
+  useNodesInitialized,
+  ConnectionLineType,
 } from "reactflow";
 import "reactflow/dist/style.css";
-import initialNodes from '../../configs/2.5.0.json';
+import dagre from "dagre";
+import initialNodes from "../../configs/2.5.0.json";
 
 const initialEdges = [
   {
@@ -26,19 +31,102 @@ const initialEdges = [
   },
 ];
 
-const fitViewOptions = {
-  padding: 1,
+const dagreGraph = new dagre.graphlib.Graph();
+dagreGraph.setDefaultEdgeLabel(() => ({}));
+
+const nodeWidth = 172;
+const nodeHeight = 36;
+
+const getLayoutedElements = (nodes: any, edges: any, direction = "TB") => {
+  const isHorizontal = direction === "LR";
+  dagreGraph.setGraph({ rankdir: direction });
+
+  nodes.forEach((node: any) => {
+    dagreGraph.setNode(node.id, { width: nodeWidth, height: nodeHeight });
+  });
+
+  edges.forEach((edge: any) => {
+    dagreGraph.setEdge(edge.source, edge.target);
+  });
+
+  dagre.layout(dagreGraph);
+
+  nodes.forEach((node: any) => {
+    const nodeWithPosition = dagreGraph.node(node.id);
+    node.targetPosition = isHorizontal ? "left" : "top";
+    node.sourcePosition = isHorizontal ? "right" : "bottom";
+
+    // We are shifting the dagre node position (anchor=center center) to the top left
+    // so it matches the React Flow node anchor point (top left).
+    node.position = {
+      x: nodeWithPosition.x - nodeWidth / 2,
+      y: nodeWithPosition.y - nodeHeight / 2,
+    };
+    console.log(node)
+    return node;
+  });
+
+  return { nodes, edges };
 };
 
+const { nodes: layoutedNodes, edges: layoutedEdges } = getLayoutedElements(
+  initialNodes,
+  initialEdges
+);
+
 const Nodes = (props: any) => {
+    const store = useStoreApi();
+    const nodesInitialized = useNodesInitialized();
   const reactFlowWrapper = useRef(null);
-  const [nodes, setNodes, onNodesChange] = useNodesState([]);
-  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+  const {setCenter } = useReactFlow();
+  const [nodes, setNodes, onNodesChange] = useNodesState(layoutedNodes);
+  const [edges, setEdges, onEdgesChange] = useEdgesState(layoutedEdges);
+
+    useEffect(() => {
+      const updatedNode = {
+        id: "my node",
+        type: "input",
+        data: { label: "input" },
+        position: { x: 0, y: 0 },
+      };
+      setNodes(() => [updatedNode]);
+    }, [setNodes]);
+
+    useEffect(() => {
+      if (nodesInitialized) {
+        const { nodeInternals } = store.getState();
+        const currNodes = Array.from(nodeInternals.values());
+        // The node printed here won't have a width or height in some cases
+        console.log(currNodes);
+      }
+    }, [nodesInitialized, store]);
+
+
   const onConnect = useCallback(
-    (params) => setEdges((eds) => addEdge(params, eds)),
+    (params) =>
+      setEdges((eds) =>
+        addEdge(
+          { ...params, type: ConnectionLineType.SmoothStep, animated: true },
+          eds
+        )
+      ),
     // eslint-disable-next-line react-hooks/exhaustive-deps
     []
   );
+    const onLayout = useCallback(
+      (direction) => {
+        const { nodes: layoutedNodes, edges: layoutedEdges } =
+          getLayoutedElements(nodes, edges, direction);
+
+        setNodes([...layoutedNodes]);
+        setEdges([...layoutedEdges]);
+      },
+      [nodes, edges]
+    );
+
+  const focusNode = (x: number, y: number, zoom: number) => {
+      setCenter(x, y, { zoom, duration: 1000 });
+  };
 
   useEffect(() => {
     setNodes([
@@ -53,10 +141,11 @@ const Nodes = (props: any) => {
         };
       }),
     ]);
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const handleNodeClick = (e, data) => {
+    onLayout("LR");
     const findChildren = nodes.filter((item) => item?.data?.parent === data.id);
     if (!findChildren.length) {
       const itemChildren = [
@@ -75,9 +164,13 @@ const Nodes = (props: any) => {
             },
             sourcePosition: "right",
             targetPosition: "left",
+            expandParent: true
           };
         }),
       ];
+      if(itemChildren.length){
+        focusNode(itemChildren[0].position.x, itemChildren[0].position.y, 1.85);
+      }
       setEdges([
         ...edges,
         ...itemChildren.map((item) => {
@@ -96,10 +189,11 @@ const Nodes = (props: any) => {
       setNodes([...nodes.filter((item) => item?.data?.parent !== data.id)]);
       setEdges([...edges.filter((item) => data.id !== item.source)]);
     }
+    // focusNode(findChildren);
   };
-const edgeTypes = {
-  smart: SmartBezierEdge,
-};
+  const edgeTypes = {
+    smart: SmartBezierEdge,
+  };
 
   return (
     <div
@@ -113,14 +207,14 @@ const edgeTypes = {
       <ReactFlow
         nodes={nodes}
         edges={edges}
-        edgeTypes = {edgeTypes}
+        edgeTypes={edgeTypes}
         onNodesChange={onNodesChange}
+        connectionLineType={ConnectionLineType.SmoothStep}
         onEdgesChange={onEdgesChange}
         onConnect={onConnect}
         onNodeClick={handleNodeClick}
         fitView
         defaultViewport={{ x: 1, y: 1, zoom: 0.5 }}
-        fitViewOptions={fitViewOptions}
       />
     </div>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,38 +1,19 @@
-import Tree from "react-d3-tree";
 import Nodes from "../components/Nodes/Nodes";
-import { useState, useCallback } from "react";
-// import "reactflow/dist/style.css";
-import initialNodes from "../configs/2.5.0.json";
-
-const useCenteredTree = (defaultTranslate = { x: 0, y: 0 }) => {
-  const [translate, setTranslate] = useState(defaultTranslate);
-  const [dimensions, setDimensions] = useState();
-  const containerRef = useCallback((containerElem) => {
-    if (containerElem !== null) {
-      const { width, height } = containerElem.getBoundingClientRect();
-      setDimensions({ width, height });
-      setTranslate({ x: width / 2, y: height / 2 });
-    }
-  }, []);
-  return [dimensions, translate, containerRef];
-};
-
-const containerStyles = {
-  width: "100vw",
-  height: "100vh",
-};
-
+// const useCenteredTree = (defaultTranslate = { x: 0, y: 0 }) => {
+//   const [translate, setTranslate] = useState(defaultTranslate);
+//   const [dimensions, setDimensions] = useState();
+//   const containerRef = useCallback((containerElem) => {
+//     if (containerElem !== null) {
+//       const { width, height } = containerElem.getBoundingClientRect();
+//       setDimensions({ width, height });
+//       setTranslate({ x: width / 2, y: height / 2 });
+//     }
+//   }, []);
+//   return [dimensions, translate, containerRef];
+// };
 
 export default function Home() {
-  const [dimensions, translate, containerRef] = useCenteredTree();
   return (
     <Nodes />
-    // <div style={containerStyles} ref={containerRef}>
-    //   <Tree
-    //     data={initialNodes}
-    //     dimensions={dimensions}
-    //     translate={translate}
-    //   />
-    // </div>
   );
 }
