fc90f8515750b7174a842b3fcf4b58aaefb8337e
acethecreator <devaze007@gmail.com>
2023-03-31 02:02:56 +0100

    creating a test deployment to netlify

--- src/components/Navigation/Sidebar.tsx
+++ src/components/Navigation/Sidebar.tsx
@@ -1,11 +1,12 @@
 import toMarkdown from "@/utils/json-to-markdown";
+import Link from "next/link";
 import React, { useEffect, useState, useRef } from "react";
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 import * as getMDXComponents from "../MDX";
 
 function Sidebar({ node, nodes, setCurrentNode }) {
-  const ref = useRef(null); 
+  const ref = useRef(null);
   const [view, setView] = useState(null);
   const [activeLabel, setActiveLabel] = useState("");
   useEffect(() => {
@@ -44,10 +45,10 @@ function Sidebar({ node, nodes, setCurrentNode }) {
                 children[0].props.children[
                   children[0].props.children.length - 1
                 ];
-                if (typeof title === "object") {
-                  title = title.props.children[0];
-                }
-              const trimedTitle = title.trim()
+              if (typeof title === "object") {
+                title = title.props.children[0];
+              }
+              const trimedTitle = title.trim();
               return (
                 <tr
                   className={`${
@@ -69,13 +70,28 @@ function Sidebar({ node, nodes, setCurrentNode }) {
     );
   }
   return (
-    <div className="side-bar p-6">
-      <h1 className="text-4xl">AsyncAPI Map</h1>
-      <p>
-        The AsyncAPI Map aims to help you find your way in the AsyncAPI
-        Specification documentation. It has been created by{" "}
-        <a href="https://twitter.com/_acebuild">Acethecreator</a>
-      </p>
+    <div className="side-bar relative">
+      <div className="p-6">
+        <h1 className="text-3xl font-bold">AsyncAPI Spec Visualizer</h1>
+        <p className="mt-6">
+          The AsyncAPI specification visualizer helps you learn and navigate the
+          different components that makes up the AsyncAPI specification in an
+          interactive way.
+        </p>
+        <div className="mt-6 bg-green-100 p-4 rounded-lg">
+          This tool currently shows the 2.5.0 and the 2.6.0 of the AsyncAPI
+          specification. You can select the version by using the dropdown at the
+          top left of the screen or also use direct link
+          <div className="text-blue-500 underline mt-2">
+            <Link href="/?version=2.5.0">version 2.5.0</Link>
+            <br />
+            <Link href="/?version=2.6.0">version 2.6.0</Link>
+          </div>
+        </div>
+      </div>
+      <div className="p-4 absolute bg-red-400 w-full bottom-0 text-xl">
+        Note: This is still a work in progress...
+      </div>
     </div>
   );
 }
